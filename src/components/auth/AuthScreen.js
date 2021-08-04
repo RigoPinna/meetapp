4a52b5012810a593d99f68d7b6f2cca9a00780f2
Rigoberto Cepeda <riguxc@gmail.com>
2021-08-04 12:14:49 -0500

    Main-project

---
+++ src/components/auth/AuthScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View } from 'react-native'
+
+export const AuthScreen = () => {
+    return (
+        <View>
+            <Text>Aqui es la ventana de autenticación</Text>
+        </View>
+    )
+}
