7c1b5f1ec1c8b92b63ed3c0ce538558b7444340e
Oscar De Leon <oscardeleonh@hotmail.com>
2022-03-15 21:02:51 -0600

    Prueba de commit (Oscar)

--- src/components/listgGroup/ListGroup.js
+++ src/components/listgGroup/ListGroup.js
@@ -38,7 +38,8 @@ export const ListGroup = ({ navigation }) => {
     return (
         <>
             {
-                (groups === STATE_GROUPS_LOADING.loading || tokenNotification===""  ) 
+                // (groups === STATE_GROUPS_LOADING.loading || tokenNotification===""  ) 
+                (groups === STATE_GROUPS_LOADING.loading ) 
                     ? <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
                     <Progress.CircleSnail spinDuration={1000} color={[COLORS_APP.primary, COLORS_APP.skyblue1]} />
                 </View>
