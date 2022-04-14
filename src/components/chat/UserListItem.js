611460e91793ca07f88dd20bdb05941f4217d8e7
Giovani <vegaavilao@gmail.com>
2022-04-13 21:04:18 -0500

    Screen EventInfo terminada

--- src/components/chat/UserListItem.js
+++ src/components/chat/UserListItem.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Image, View, Text } from 'react-native';
 import { styleListGroups } from '../../theme/appTheme'
 import { Textapp } from '../elements/Textapp';
